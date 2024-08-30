560977c923e98f04db1817b2ae0aaa381d23273d
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2024-08-31 01:36:17 +0530

    Navbar completed

---
+++ kitchen_website/src/components/Home.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <div>
+            <img className='max-w-170' src="https://plus.unsplash.com/premium_photo-1680382578857-c331ead9ed51?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ kitchen_website/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { CiSearch } from "react-icons/ci";
+import { RiAccountCircleLine } from "react-icons/ri";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+
+function Navbar() {
+    return (
+        <div>
+            <div className='flex justify-between p-9 items-center min-h-24'>
+                <img src="https://demo2.themelexus.com/kitchor/wp-content/uploads/2022/06/logo.svg" alt="" />
+                <div className='lg:space-x-9  text-sm font-semibold'>
+                    <button className='hover:text-[#DDA859]'>HOME</button>
+                    <button className='hover:text-[#DDA859]'>VIRTUAL TOURS</button>
+                    <button className='hover:text-[#DDA859]'>PROJECT</button>
+                    <button className='hover:text-[#DDA859]'>PAGES</button>
+                    <button className='hover:text-[#DDA859]'>SHOP</button>
+                    <button className='hover:text-[#DDA859]'>BLOG</button>
+                    <button className='hover:text-[#DDA859]'>CONTACT</button>
+                </div>
+                <div className='space-x-7 text-2xl '>
+                    <button className='hover:text-[#DDA859]'><CiSearch /></button>
+                    <button className='hover:text-[#DDA859]'><RiAccountCircleLine /></button>
+                    <button className='hover:text-[#DDA859]'><HiOutlineShoppingBag /></button>
+                    <button className='hover:text-[#DDA859]'><RxHamburgerMenu /></button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
